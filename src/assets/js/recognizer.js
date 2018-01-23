8bc6c942617b2ce123853356e86e1d43ae1e0b85
François Desrichard <git@desrichard.fr>
2018-01-23 13:49:18 +0100

    Added comments - created recognizer

---
+++ src/assets/js/recognizer.js
@@ -0,0 +1,9 @@
+/**
+ * Recognizes a given touch gesture
+ * @param touchArray {Array} The array of finger positions
+ * @return {String} The recognized gesture's identifier
+ */
+export const recognizeGesture = (touchArray) => {
+	
+	return "";
+};
